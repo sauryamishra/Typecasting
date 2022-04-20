c6d31e477c1b2d932035e57fbda6da694a3f2ea6
Saurya mishra <60350420+sauryamishra@users.noreply.github.com>
2022-04-20 21:10:09 +0530

    Add files via upload

---
+++ typeCasting.java
@@ -0,0 +1,14 @@
+
+public class typeCasting {
+
+	public static void main(String[] args) {
+
+	double myDouble = 12.12;
+	int myInt = (int)myDouble;
+	System.out.println(myDouble);
+		System.out.println(myInt);
+		
+		
+
+}
+}
